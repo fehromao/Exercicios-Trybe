741ccf863c2c25cbe2eedc1d154922bc0993cc03
Fernanda Romao <Fernanda Romao>
2022-02-02 18:51:22 -0300

    Fiz o exercicio 1 do dia 4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,7 @@
+const a = 10;
+const b = 3;
+console.log("adição" +(a + b));
+console.log("subtração" +(a - b));
+console.log("multiplicação"+(a * b));
+console.log("divisão"+(a / b));
+console.log("modulo"+(a % b));

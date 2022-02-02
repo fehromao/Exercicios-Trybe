990abcfe5198036a5aa9ac81356d7fd5bc753a52
Fernanda Romao <Fernanda Romao>
2022-02-02 20:40:37 -0300

    Fiz o exercicio 3 do dia 4.1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,9 +1,32 @@
-const a = 28;
-const b = 12;
+// const a = 10;
+// const b = 3;
+// console.log("adição" + (a + b));
+// console.log("subtração" + (a - b));
+// console.log("multiplicação"+ (a * b));
+// console.log("divisão"+ (a / b));
+// console.log("modulo"+ (a % b));
 
-if(a > b) {
-  console.log("a é maior que b");
+// const a = 28;
+// const b = 12;
+
+// if(a > b) {
+//   console.log("a é maior que b");
+// }
+// else{
+//   console.log("b é menor que a");
+// }
+
+const a = 75;
+const b = 300;
+const c = 100;
+
+if(a>b && a>c){
+  console.log("o maior numero é a");
 }
-else{
-  console.log("b é menor que a");
+else if (b>c && b>a) { 
+  console.log ("o maior numero é b")
+}
+
+else {
+  console.log ("o maior numero é c")
 }

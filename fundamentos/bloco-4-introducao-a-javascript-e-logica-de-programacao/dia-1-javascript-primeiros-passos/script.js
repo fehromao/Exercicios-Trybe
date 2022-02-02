71a384894f970aea6318595521c94c4e577c9176
Fernanda Romao <Fernanda Romao>
2022-02-02 20:51:43 -0300

    Fiz o exercicio 4 do dia 4.1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -16,17 +16,26 @@
 //   console.log("b é menor que a");
 // }
 
-const a = 75;
-const b = 300;
-const c = 100;
+// const a = 75;
+// const b = 300;
+// const c = 100;
 
-if(a>b && a>c){
-  console.log("o maior numero é a");
-}
-else if (b>c && b>a) { 
-  console.log ("o maior numero é b")
-}
+// if(a>b && a>c){
+//   console.log("o maior numero é a");
+// }
+// else if (b>c && b>a) { 
+//   console.log ("o maior numero é b")
+// }
 
-else {
-  console.log ("o maior numero é c")
+// else {
+//   console.log ("o maior numero é c")
+// }
+
+const a = 10;
+if (a > 0){
+  console.log("positive")
+} else if(a < 0){
+  console.log("negative")
+} else {
+  console.log("zero")
 }

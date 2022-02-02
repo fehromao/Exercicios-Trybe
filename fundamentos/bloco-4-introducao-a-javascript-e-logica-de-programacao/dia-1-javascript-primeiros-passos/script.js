93aeb1db7d40e03899204303b49f25a4f83f73e7
Fernanda Romao <Fernanda Romao>
2022-02-02 19:09:31 -0300

    Fiz o exercicio 2 do dia 4.1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,7 +1,9 @@
-const a = 10;
-const b = 3;
-console.log("adição" +(a + b));
-console.log("subtração" +(a - b));
-console.log("multiplicação"+(a * b));
-console.log("divisão"+(a / b));
-console.log("modulo"+(a % b));
+const a = 28;
+const b = 12;
+
+if(a > b) {
+  console.log("a é maior que b");
+}
+else{
+  console.log("b é menor que a");
+}

53083424b20d418934e3537d84679a810ac632ab
Fernanda Romao <Fernanda Romao>
2022-02-03 19:32:55 -0300

    Fiz o exercicio 4 do dia 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -10,3 +10,10 @@ for( let index = 0; index < numbers.length; index += 1){
   }
 // console.log(soma);
 console.log(soma / numbers.length)
+
+if( soma / numbers.length > 20) {
+      console.log("Valor maior que 20")
+    }
+    else {
+      console.log("Valor menor ou igual a 20!")  
+    }

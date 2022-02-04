93dc58437ed4bc388246a1008c5e4bddbfe6e935
Fernanda Romao <Fernanda Romao>
2022-02-04 18:46:25 -0300

    Fiz o exercicio 8 do dia 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -57,12 +57,21 @@
 
 // 7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorValor = numbers[0];
-for(index = 1;index < numbers.length; index +=1){
-  if ( numbers[index] < menorValor) {
-    menorValor = numbers[index];
-  }
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let menorValor = numbers[0];
+// for(index = 1;index < numbers.length; index +=1){
+//   if ( numbers[index] < menorValor) {
+//     menorValor = numbers[index];
+//   }
+// }
 
-console.log(menorValor)
+// console.log(menorValor)
+
+// 8 - Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numeros = [];
+for (let index = 1; index <= 25; index+=1) {
+  numeros.push(index);
+  
+}
+console.log(numeros);

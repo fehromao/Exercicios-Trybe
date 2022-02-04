17ea57f8a4b5c58737b67d6bc64c8fc9f5900b46
Fernanda Romao <Fernanda Romao>
2022-02-04 18:36:37 -0300

    Fiz o exercico 7 do dia 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -40,29 +40,29 @@
 //   console.log (maiorNumero)
 // }
 
-5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+// 5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorValor = numbers[0];
-for (let i = 0; i < numbers.length; i +=1) {
-  if( numbers[i] > maiorValor) {
-    maiorValor = numbers[i]
-  }
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maiorValor = numbers[0];
+// for (let i = 0; i < numbers.length; i +=1) {
+//   if( numbers[i] > maiorValor) {
+//     maiorValor = numbers[i]
+//   }
   
-}
-console.log(maiorValor);
+// }
+// console.log(maiorValor);
 
 
 
 // 7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let menorValor = numbers[0];
-// for(index = 1;index < numbers.length; index +=1){
-//   if ( numbers[index] < menorValor) {
-//     menorValor = numbers[index];
-//   }
-// }
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorValor = numbers[0];
+for(index = 1;index < numbers.length; index +=1){
+  if ( numbers[index] < menorValor) {
+    menorValor = numbers[index];
+  }
+}
 
-// console.log(menorValor)
+console.log(menorValor)

d7fcfd6de7b483a74429e02bd4be240c73231ac7
Fernanda Romao <Fernanda Romao>
2022-02-04 14:55:36 -0300

    Fiz o exercicio 6 do dia 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,19 +1,36 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for( let index = 0; index < numbers.length; index += 1){
 //   console.log(numbers[index])
 // }
 
-let soma = 0;
-for( let index = 0; index < numbers.length; index += 1){
-    soma += numbers[index]
-  }
-// console.log(soma);
-console.log(soma / numbers.length)
+// let soma = 0;
+// for( let index = 0; index < numbers.length; index += 1){
+//     soma += numbers[index]
+//   }
+// // console.log(soma);
+// console.log(soma / numbers.length)
 
-if( soma / numbers.length > 20) {
-      console.log("Valor maior que 20")
-    }
-    else {
-      console.log("Valor menor ou igual a 20!")  
-    }
+// if( soma / numbers.length > 20) {
+//       console.log("Valor maior que 20")
+//     }
+//     else {
+//       console.log("Valor menor ou igual a 20!")  
+//     }
+
+// let impares = []
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = []
+for (let index = 0; index < numbers.length; index+=1) {
+ if (numbers[index] % 2 !== 0 ) {
+  impar +=1
+ }  
+}
+if( impar === 0){
+  console.log("Nenhum valor encontrado")
+}
+else{
+  console.log(impar)
+}
+
